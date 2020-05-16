e345e21832a5d2cd3a2fdb700f60747b6027b188
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-16 21:15:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   
   attr_accessor :name, :breed, :id 
   
-  def initialize(name:, breed:, id=NIL)
+  def initialize(name:, breed:, id= NIL)
     @name = name
     @breed = breed
     @id = id 
